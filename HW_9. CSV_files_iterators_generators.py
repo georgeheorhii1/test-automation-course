7d4308c0689d7fee10ef8092787e4f765447fa9a
georgeheorhii1 <112317335+georgeheorhii1@users.noreply.github.com>
2023-04-09 16:37:49 +0300

    Add files via upload
    I Create a function that reads a CSV file. It accepts the name of the file (str), returns a list of strings (list). Also create a function that writes data to a file. It accepts the name of the file (str), a list of strings (list) that need to be written to the file. Does not return anything. Now, using the created functions, first create a file (3 lines are enough), then read the same file, write the lines in a variable, then add two lines to the variable, and then write your changes in the second file.
    
    II Create a function that returns the squares of all numbers in the range 0 - 100000.
    It should work and not put your computer down.

---
+++ HW_9. CSV_files_iterators_generators.py
@@ -0,0 +1,47 @@
+import csv
+
+
+# Task_1
+def read_csv_file(file_name):
+    """
+    Reads a CSV file and returns a list of strings.
+    """
+    with open(file_name, 'r') as file:
+        reader = csv.reader(file)
+        lines = []
+        for line in reader:
+            lines.append(','.join(line))
+        return lines
+
+
+def write_to_csv_file(file_name, lines):
+    """
+    Writes a list of strings to a CSV file.
+    """
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for line in lines:
+            writer.writerow(line.split(','))
+
+
+lines = ['1,2,3', '4,5,6', '7,8,9']
+write_to_csv_file('file1.csv', lines)
+
+new_lines = read_csv_file('file1.csv')
+
+new_lines.append('10,11,12')
+new_lines.append('13,14,15')
+
+write_to_csv_file('file2.csv', new_lines)
+
+
+# Создайте функцию, которая возвращает квадраты всех чисел в диапазоне 0 – 100000.
+# Должен сработать и не положить ваш компьютер.
+def find():
+    for i in range(100001):
+        yield i ** 2
+
+
+squares = find()
+for i in range(100):
+    print(next(squares))
